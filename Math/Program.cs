bf70ce1fdf443738fb2cd371ef702e279a359a19
MeisterYM <yannick_mart@yahoo.com>
2024-05-13 16:16:59 -0500

    First commit

---
+++ Math/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Math {
+    class Program {
+        static void Main(string[] args) {
+
+            int number = 1;
+
+            float other_number = Convert.ToSingle(number);
+
+            Console.WriteLine(other_number.GetType());
+        }
+    }
+}
