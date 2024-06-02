a1d7f6b5b69dfeff3d8b20634194af90a188d68e
MeisterYM <yannick_mart@yahoo.com>
2024-06-02 09:06:43 -0500

    Added Linear_Search

---
+++ Linear_Search/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Linear_Search {
+
+    class Program {
+
+        static void Main(string[] args) {
+//Initialize an array and give it values.
+int[] thing = {1, 2, 3, 5, 4, 7, 8, 9, 6, 10};
+
+//The number that the algorithm is going to look for.
+int lookup = 6;
+
+//Finding the size of the array before running the loop since for some reason using thing.Length in the for loop causes problems.
+int size = thing.Length;
+
+//Commented code to test variables and methods.
+//Console.WriteLine(size);
+
+//Linear search implementation. Tells you when value is found and where in the array. Otherwise says nothing.
+for(int i = 0; i < size; i++) {
+    if(lookup == thing[i]) {
+        Console.WriteLine("Found it! The index is at " + i + ".");
+    }
+}
+        }
+    }
+}
